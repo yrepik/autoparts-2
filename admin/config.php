41ddda12e38e502210b00eea07df895209f6fc1e
mehmetmertyildiran@gmail.com <admin@server2>
2015-03-20 18:03:22 -0400

    Installation completed.

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://autoparts.mertyildiran.com/admin/');
+define('HTTP_CATALOG', 'http://autoparts.mertyildiran.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://autoparts.mertyildiran.com/admin/');
+define('HTTPS_CATALOG', 'http://autoparts.mertyildiran.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/admin/web/autoparts.mertyildiran.com/public_html/admin/');
+define('DIR_SYSTEM', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/');
+define('DIR_LANGUAGE', '/home/admin/web/autoparts.mertyildiran.com/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/admin/web/autoparts.mertyildiran.com/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/config/');
+define('DIR_IMAGE', '/home/admin/web/autoparts.mertyildiran.com/public_html/image/');
+define('DIR_CACHE', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/download/');
+define('DIR_UPLOAD', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/upload/');
+define('DIR_LOGS', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/logs/');
+define('DIR_MODIFICATION', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/modification/');
+define('DIR_CATALOG', '/home/admin/web/autoparts.mertyildiran.com/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin_autoparts');
+define('DB_PASSWORD', 'oQdArzT5b2');
+define('DB_DATABASE', 'admin_autoparts');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://autoparts.mertyildiran.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://autoparts.mertyildiran.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/admin/web/autoparts.mertyildiran.com/public_html/catalog/');
+define('DIR_SYSTEM', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/');
+define('DIR_LANGUAGE', '/home/admin/web/autoparts.mertyildiran.com/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/admin/web/autoparts.mertyildiran.com/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/config/');
+define('DIR_IMAGE', '/home/admin/web/autoparts.mertyildiran.com/public_html/image/');
+define('DIR_CACHE', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/download/');
+define('DIR_UPLOAD', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/upload/');
+define('DIR_MODIFICATION', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/modification/');
+define('DIR_LOGS', '/home/admin/web/autoparts.mertyildiran.com/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin_autoparts');
+define('DB_PASSWORD', 'oQdArzT5b2');
+define('DB_DATABASE', 'admin_autoparts');
+define('DB_PREFIX', 'oc_');
